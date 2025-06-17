d1e7ab555233af2ba59297500841186007350e34
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-06-17 11:08:16 +0545

    Checkboxes in Python. Checkbox in Python GUI, when ticked I like food and when unticked I don't like the food message displayed in the terminal.

---
+++ 71_PyQt5_Checkboxes.py
@@ -0,0 +1,36 @@
+# PyQt5 CheckBoxes
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QCheckBox
+from PyQt5.QtCore import Qt
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 300, 500, 500)
+        self.checkbox = QCheckBox("Do you like food?", self)
+        self.initUI()
+        
+    def initUI(self):
+        self.checkbox.setGeometry(10, 0, 500, 100)
+        self.checkbox.setStyleSheet("font-size: 30px;" 
+                                    "font-family: Arial;")
+        # Default brhaviour of checkbox is unchecked
+        # self.checkbox.setChecked(True) # Makes it check by default
+        self.checkbox.setChecked(False)
+        
+        self.checkbox.stateChanged.connect(self.checkbox_changed)
+        
+    def checkbox_changed(self, state):
+        # state (2 means checked and 0 means unchecked)
+        # print(state)
+        if state == Qt.Checked:     
+            print("You like Food")
+            
+        else:
+            print("You do not like Food") 
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
