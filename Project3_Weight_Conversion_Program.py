7f6cc0c938877fb2b7e5ae30fc841c260e7037c9
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-06-19 10:50:35 +0545

    Weight Conversion Program in Python. Kilogram to Pounds and Pounds to kilogram.

---
+++ Project3_Weight_Conversion_Program.py
@@ -0,0 +1,18 @@
+# Python weight converter
+
+weight = float(input("Enter your weight: "))
+unit = input("What unit of weight did you entered Kilograms or Pounds? (K or L): ")
+
+if unit == "K" or "k":
+    weight = weight * 2.205
+    unit = "Lbs."
+    print(f"Your weight is: {round(weight, 1)} {unit}")
+    
+elif unit == "L" or "l":
+    weight = weight / 2.205
+    unit = "Kgs."
+    print(f"Your weight is: {round(weight, 1)} {unit}")
+
+else:
+    print(f"{unit} was not valid")
+    
