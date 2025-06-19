2549d607f3e72d875454d2673cb310c86ccda32e
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-06-19 10:40:51 +0545

    Matlibs Game made in Python. Inserting random words and verbs and the displaying a random story. Fun to play.

---
+++ Project1_Matlibs_Game.py
@@ -0,0 +1,33 @@
+# Matlibs game
+# word game where you create a story
+# by filling in blanks with random words
+"""
+adjective1 = input("Enter an Adjective (description, eg: spiderman): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an Adjective (description of something): ")
+verb1 = input("Enter a verb ending with 'ing': ")
+adjective3 = input("Enter an Adjective (description): ")
+
+print("\n")
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exhibit, I saw {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
+"""
+
+# -----Another matlibs game made by Anish Khatri -----
+print("\n")
+dream = input("Enter anything related to Fantasy: ")
+type = input("Short description about that Fantasy: ")
+verb = input("Enter a verb ending with 'ing': ")
+mood = input("Enter your mood (eg, funny, scary, awesome): ")
+hero = input("Enter a superhero name: ")
+final = input("Enter a funny word ending with 'ing' ... : ")
+
+print("\n")
+print(f"Today, I saw {dream} in my dream")
+print(f"It was, {type} !!!")
+print(f"It was {verb} with me.")
+print(f"I find my {dream} very {mood}")
+print(f"I wish I can see {hero} instead of {dream} in my dream")
+print(f"At the end I was {final}")
