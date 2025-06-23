46ee5f12e1eedcd870584648930669d92cb271c5
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-06-23 11:13:51 +0545

    Wordlists for Hangman Game. Random words are taken from this Tuple for the Game.

---
+++ Project16_Wordslist.py
@@ -0,0 +1,16 @@
+# words for hangman game
+
+words = ("apple", "banana", "basket", "breeze", "bridge", "butterfly", "candle", "carrot", "castle",
+    "cloud", "crystal", "dancing", "diamond", "dolphin", "dragon", "dream", "eagle", "echo",
+    "emerald", "energy", "feather", "firefly", "fortune", "fountain", "garden", "gentle", "glacier",
+    "golden", "grizzly", "harmony", "horizon", "island", "jungle", "lantern", "lightning",
+    "lighthouse", "lively", "magnet", "marble", "meadow", "memory", "moonlight", "mountain",
+    "mystery", "nightfall", "ocean", "opal", "paradise", "pebble", "penguin", "phoenix", "pineapple",
+    "pirate", "planet", "platinum", "puzzle", "quicksand", "rainbow", "raindrop", "riddle",
+    "riverbank", "sapphire", "season", "shimmer", "skyline", "snowflake", "sparkle", "spiral",
+    "starfish", "stormy", "sunbeam", "sunflower", "sunset", "sunshine", "symphony", "teardrop",
+    "thunder", "tornado", "treasure", "trickster", "twilight", "umbrella", "unicorn", "valley",
+    "velvet", "village", "vortex", "wandering", "whale", "whisper", "wildflower", "willow",
+    "windmill", "wingspan", "wonderland", "zephyr", "zigzag", "adventure", "balloon", "boulder",
+    "bubble", "canyon", "compass", "dreamer", "echoes"
+)
